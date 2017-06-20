3d475b87269cf34f54cd928289e153549c86e5c1
agzuniverse <aswingauthor@gmail.com>
2017-06-20 16:12:44 +0530

    added messengerbot files

---
+++ MESSENGERBOT/index.js
@@ -0,0 +1,56 @@
+'use strict'
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+/* For Facebook Validation */
+app.get('/webhook/', (req, res) => {
+  if (/*req.query['hub.mode'] && */req.query['hub.verify_token'] === 'z_bot') {
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    res.status(403).end();
+  }
+});
+
+/* Handling all messenges */
+app.post('/webhook/', (req, res) => {
+  console.log(req.body);
+  if (req.body.object === 'page') {
+    req.body.entry.forEach((entry) => {
+      entry.messaging.forEach((event) => {
+        if (event.message && event.message.text) {
+          sendMessage(event);
+        }
+      });
+    });
+    res.status(200).end();
+  }
+});
+
+function sendMessage(event) {
+  let sender = event.sender.id;
+  let text = event.message.text;
+
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:  'EAAGIxK5fpXcBADZChDN7iQo6JMxr5biHm9ZBDhT49Cn8yUEOZB8JOcqxpVisD1qFmGwZBRIQf3o4R7fK695y63J2tibMV91VBU7XxEtGI8BldZAOv1bvNlpwa6Y4OXgkDTSLW6zZBG8QUl11BZB4mclNwcWtddoMXSQ7ZAZCLqnITugZDZD'},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: {text: text}
+    }
+  }, function (error, response) {
+    if (error) {
+        console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+    }
+  });
+}
