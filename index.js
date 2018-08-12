be8a6103ac6f496e71287b88c078f5fe2f2c25c2
Aswin G <aswinganesh666@gmail.com>
2018-08-12 18:26:03 +0530

    Add typed.js awesomeness and social links

--- index.js
+++ index.js
@@ -2,4 +2,21 @@ $(document).ready(() => {
   particlesJS.load("particles", "particles-config.json", () => {
     // console.log("particles.js loaded");
   });
+
+  var options = {
+    strings: [
+      "I like to code",
+      "I like to write",
+      "I like to play",
+      "I like to listen"
+    ],
+    typeSpeed: 50,
+    startDelay: 500,
+    backSpeed: 35,
+    backDelay: 500,
+    loop: true,
+    loopCount: Infinity
+  };
+
+  var typed = new Typed(".subtitle", options);
 });
